3f243fbb6ec3486dc46886dfdcf2569b2a57f667
Mayank Jha <mayankjha7722@gmail.com>
2018-12-26 19:53:27 +0530

    home page bug fixed!!

--- mangobyte/src/app/home/home.component.ts
+++ mangobyte/src/app/home/home.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+declare var $:any;
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements AfterViewInit  {
 
   constructor() { }
 
-  ngOnInit() {
+  ngAfterViewInit() {
+    $.getScript('../assets/js/main.js');
   }
 
 }
