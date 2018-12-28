4890e7e113b6e42311882ede96f3c8265b8619ab
Govind Singh <govind.resarch0000@rediffmail.com>
2018-12-29 00:54:56 +0530

    blog started

---
+++ mangobyte/src/app/blog/blog.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-blog',
+  templateUrl: './blog.component.html',
+  styleUrls: ['./blog.component.scss']
+})
+export class BlogComponent implements OnInit {
+  @Input() do:any;
+  constructor() {
+
+  }
+
+  ngOnInit() {
+  }
+
+}
