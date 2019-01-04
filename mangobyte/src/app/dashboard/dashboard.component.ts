12e0f734739553fd938b76ec557d9e8677c21c36
Govind Singh <govind.resarch0000@rediffmail.com>
2019-01-04 15:05:59 +0530

    dashboard made dynamic

--- mangobyte/src/app/dashboard/dashboard.component.ts
+++ mangobyte/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { LoginService } from '../login/login.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private ls:LoginService, private cdr:ChangeDetectorRef) { }
+  nav_list=[
+    ["home", "home"],
+    ["Interviews", "chalkboard-teacher"],
+    ["Courses", "user-graduate"],
+    ["Logout", "power-off"]  
+  ]
+  nav_selected=this.nav_list[0][0]
+  updatenav(nav){
+    this.nav_selected=nav[0];
+    this.cdr.detectChanges();
+    //console.log(this.nav_selected, nav);
+  }
   ngOnInit() {
+    console.log()
   }
 
 }
